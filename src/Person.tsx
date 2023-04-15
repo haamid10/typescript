4fd83936d975613fea59b3a0f714353696f9e5dc
haamid10 <apdixamiid919@gmail.com>
2023-04-15 23:02:37 +0300

    ChabgrEvent

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,4 +1,4 @@
-import {FC,useState} from 'react';
+import {FC,ChangeEvent,useState} from 'react';
 
  interface Siu {
   name: string;
@@ -10,11 +10,19 @@ import {FC,useState} from 'react';
 export const Person: FC<Siu> = ({name,email, age}) =>  {
 
   const [country, setCountry] = useState<string | null>(null)
+
+  const handleChange =(event: ChangeEvent<HTMLInputElement>)=>{
+    setCountry(event.target.value)
+  }
   return (
     <div> 
       <h2>Name: {name}</h2>
       <h2>email: {email}</h2>
       <h2>Age: {age}</h2>
+
+      <input type="text" placeholder='write  sown your country' onChange={handleChange} />
+
+      {country}
     </div>
   );
 }
